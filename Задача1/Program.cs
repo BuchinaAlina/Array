06088a7e38270a6ff2c04e211025e24ae47109f8
Alina <buchina-alina@mail.ru>
2023-10-26 18:41:33 +1000

    pfl

--- Задача1/Program.cs
+++ Задача1/Program.cs
@@ -1,15 +1,24 @@
 ﻿//Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
 //массив [10 21 14 93 23] => 2
 
+
 using System;
 
 class Program
 {
     static void Main()
     {
-        int[] array = new int[] { 10, 21, 14, 93, 23, 45, 70, 88, 99, 12 };
-        int count = 0;
+        int[] array = new int[10];
+        Random random = new Random();
 
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(1, 101); // Генерируем случайное целое число от 1 до 100
+        }
+
+        Console.WriteLine("Сгенерированный массив: [" + string.Join(" ", array) + "]");
+        
+        int count = 0;
         foreach (int num in array)
         {
             if (num >= 20 && num <= 90)
@@ -18,7 +27,6 @@ class Program
             }
         }
 
-        Console.WriteLine("Массив: [" + string.Join(" ", array) + "]");
         Console.WriteLine("Количество элементов в диапазоне [20, 90]: " + count);
     }
 }
