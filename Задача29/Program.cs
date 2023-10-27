b996ebe4475d3dce84e670673a09ba1e47a91e46
Alina <buchina-alina@mail.ru>
2023-10-27 16:37:21 +1000

    we

---
+++ Задача29/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите элементы массива (разделите числа запятыми): ");
+        string input = Console.ReadLine();
+        string[] inputElements = input.Split(',');
+
+        int[] numbers = new int[inputElements.Length];
+
+        for (int i = 0; i < inputElements.Length; i++)
+        {
+            numbers[i] = int.Parse(inputElements[i]);
+        }
+
+        Console.Write("Массив: [");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i]);
+            if (i < numbers.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+    }
+}
+
